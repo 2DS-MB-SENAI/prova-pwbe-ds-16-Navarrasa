fcc8c1db58fa3c06ec2ac7ae40dede39f70da769
Gustavo Bruno de Paiva Florentino <49169015848@>
2025-04-08 10:20:33 -0300

    renomeando variaveis

--- agenda/models.py
+++ agenda/models.py
@@ -12,7 +12,7 @@ class Servico(models.Model):
     
 class Agendamento(models.Model):
     servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
-    data = models.DateTimeField()
+    data_hora = models.DateTimeField()
     cliente_nome = models.CharField(max_length=100)
     cliente_email = models.EmailField()
 
