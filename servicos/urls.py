ff2e7c6241a59817579f16f6fa6b460d6f0f1bb4
Gustavo Bruno de Paiva Florentino <49169015848@>
2025-04-08 09:46:41 -0300

    url updates

--- servicos/urls.py
+++ servicos/urls.py
@@ -22,7 +22,7 @@ Setando as URLS principais do servidor Django (url raíz)
 """
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('clinica/', include('clinica.urls')),
+    path('/', include('clinica.urls')),
 ]
     # path('auth/', include('custom_auth.urls')),
     # path('agenda/', include('agenda.urls')),
